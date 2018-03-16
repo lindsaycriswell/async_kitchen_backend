e934ea4bdee027f0baa0d90c005c5d68e9c21eca
github username <github email address>
2018-03-16 11:18:05 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -67,10 +67,94 @@ RECIPES = [
     {description: 'Cube potatoes into small bite sized pieces, place potatoes into a gallon sized ziplock bag, pour oil over potatoes, and mix well. Add in optional ingredients if you like. Sprinkle in dry onion soup mix, and shake the back so the soup mix coats the potatoes.', time: 9, do_ahead: true},
     {description: 'Place potatoes into a 13 x 9 inch baking dish. Bake for approximately 40 minutes.', time: 40, do_ahead: false}
     ]
+  },
+
+  {name: 'Asian Salmon in Foil',
+  course: 'main',
+  servings: '4',
+  prep_time: 10,
+  cook_time: 20,
+  total_time: 30,
+  url: 'https://www.yummly.com/#recipe/Asian-Salmon-in-Foil-2283535',
+  ingredients: [
+    {description: '1/4 cup honey', search_name: 'honey'},
+    {description: '3 cloves garlic, minced', search_name: 'garlic'},
+    {description: '2 tablespoons reduced sodium soy sauce', search_name: 'soy sauce'},
+    {description: '1 tablespoon seasoned rice vinegar', search_name: 'rice vinegar'},
+    {description: '1 tablespoon sesame oil', search_name: 'sesame oil'},
+    {description: '1 tablespoon freshly grated ginger', search_name: 'ginger'},
+    {description: '1 teaspoon Sriracha, optional', search_name: 'sriracha'},
+    {description: 'Freshly ground black pepper, to taste', search_name: 'black pepper'},
+    {description: '2 pounds salmon', search_name: 'salmon'},
+    {description: '2 green onions, thinly sliced', search_name: 'green onion'},
+    {description: '1/2 teaspoon sesame seeds', search_name: 'sesame seeds'}
+  ],
+  directions: [
+    {description: 'Preheat oven to 375 degrees F. Line a baking sheet with foil.', time: 2, do_ahead: true},
+    {description: 'In a small bowl, whisk together honey, garlic, soy sauce, rice vinegar, sesame oil, ginger, Sriracha and pepper, to taste.', time: 3, do_ahead: true},
+    {description: 'Place salmon onto the prepared baking sheet and fold up all 4 sides of the foil. Spoon the honey mixture over the salmon. Fold the sides of the foil over the salmon, covering completely and sealing the packet closed.', time: 5, do_ahead: false},
+    {description: 'Place into oven and bake until cooked through, about 15-20 minutes.', time: 17, do_ahead: false},
+    {description: 'Open the packet and broil for 2-3 minutes, or until caramelized and slightly charred.', time: 2, do_ahead: false},
+    {description: 'Serve immediately, garnished with green onions and sesame seeds, if desired.', time: 1, do_ahead: false}
+    ]
+  },
+
+  {name: 'Easy Lo Mein',
+  course: 'side',
+  servings: '4',
+  prep_time: 10,
+  cook_time: 15,
+  total_time: 25,
+  url: 'https://www.yummly.com/#recipe/Easy-Lo-Mein-2281128',
+  ingredients: [
+    {description: '8 ounces lo mein egg noodles', search_name: 'egg noodles'},
+    {description: '1 tablespoon olive oil', search_name: 'olive oil'},
+    {description: '2 cloves garlic, minced', search_name: 'garlic'},
+    {description: '2 cups cremini mushrooms, sliced', search_name: 'mushrooms'},
+    {description: '1 red bell pepper, julienned', search_name: 'bell pepper'},
+    {description: '1 carrot, julienned', search_name: 'carrot'},
+    {description: '1/2 cup snow peas', search_name: 'snow peas'},
+    {description: '3 cups baby spinach', search_name: 'spinach'},
+    {description: '2 tablespoons reduced sodium soy sauce, or more, to taste', search_name: 'soy sauce'},
+    {description: '2 teaspoons sugar', search_name: 'sugar'},
+    {description: '1 teaspoon sesame oil', search_name: 'sesame oil'},
+    {description: '1/2 teaspoon ground ginger', search_name: 'ginger'},
+    {description: '1/2 teaspoon Sriracha, or more, to taste', search_name: 'sriracha'}
+  ],
+  directions: [
+    {description: 'Chop garlic, mushrooms, bell pepper, and carrot', time: 7, do_ahead: true},
+    {description: 'In a small bowl, whisk together soy sauce, sugar, sesame oil, ginger and Sriracha; set aside.', time: 3, do_ahead: true},
+    {description: 'In a large pot of boiling water, cook noodles according to package instructions; drain well.', time: 8, do_ahead: false},
+    {description: 'Heat olive oil in a large skillet or wok over medium high heat. Add garlic, mushrooms, bell pepper and carrot. Cook, stirring frequently, until tender, about 3-4 minutes.', time: 4, do_ahead: false},
+    {description: 'Stir in snow peas and spinach until the spinach has wilted, about 2-3 minutes.', time: 2, do_ahead: false},
+    {description: 'Stir in egg noodles and soy sauce mixture, and gently toss to combine.', time: 1, do_ahead: false}
+    ]
+  },
+
+  {name: '5 Ingredient Coconut Curry',
+  course: 'main',
+  servings: '3-4',
+  prep_time: 5,
+  cook_time: 10,
+  total_time: 15,
+  url: 'https://pinchofyum.com/5-ingredient-coconut-curry?utm_campaign=yummly&utm_medium=yummly&utm_source=yummly',
+  ingredients: [
+    {description: '1 can coconut milk', search_name: 'coconut milk'},
+    {description: '2 tablespoons red curry paste', search_name: 'curry paste'},
+    {description: '2 small heads broccoli (and/or other veggies of choice)', search_name: 'broccoli'},
+    {description: '1 can chickpeas, rinsed and drained', search_name: 'chickpeas'},
+    {description: '1/2 tablespoon cornstarch dissolved in 2 tablespoons cold water', search_name: 'cornstarch'},
+    {description: 'optional: minced garlic or onion', search_name: 'garlic'}
+  ],
+  directions: [
+    {description: 'Saute broccoli (and onion/garlic if you’re using it) in a tablespoon of oil.', time: 3, do_ahead: false},
+    {description: 'After a few minutes, add the coconut milk and let simmer for 5-8 minutes. The broccoli should soften but still be tender-crisp.', time: 7, do_ahead: false},
+    {description: 'Add the curry paste to the pan and whisk it until it combines with the coconut milk. Add the chickpeas.', time: 1, do_ahead: false},
+    {description: 'Bring to a slight boil and add the cornstarch.', time: 3, do_ahead: false},
+    {description: 'Boil for about a minute, then reduce heat and let cool slightly. Sauce will thicken as the mixture cools.', time: 1, do_ahead: false}
+    ]
   }
 ]
-
-
 
 
 def createRecipes
