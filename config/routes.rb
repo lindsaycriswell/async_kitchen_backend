b91e4a045abb3f79ce1f507366688d1f78c91a0b
github username <github email address>
2018-03-17 14:17:20 -0400

    updated recipe meals controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # resources :users
   resources :meals
-  resources :recipe_meals, only: [:create, :update, :destroy]
+  resources :recipe_meals, only: [:show, :create, :update, :destroy]
   # resources :directions
   # resources :ingredients
   resources :recipes, only: [:index, :show]
