0b110836c025204934cd7dd84bbf610eb8d879d2
lindsaycriswell <lindsaycriswell@gmail.com>
2018-05-06 17:29:21 -0400

    cleaned up meal routes and methods

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,26 +1,26 @@
 class MealsController < ApplicationController
-  before_action :set_meal, only: [:update, :destroy]
+  # before_action :set_meal, only: [:update, :destroy]
 
-  def index
-    meals = Meal.all
-    render json: meals, status: 200
-  end
+  # def index
+  #   meals = Meal.all
+  #   render json: meals, status: 200
+  # end
 
   def create
     meal = Meal.create(meal_params)
     render json: meal, status: 201
   end
 
-  def update
-    @meal.update(meal_params)
-    render json: @meal, status: 200
-  end
-
-  def destroy
-    mealId = @meal.id
-    @meal.destroy
-    render json: {message:"Meal deleted"}
-  end
+  # def update
+  #   @meal.update(meal_params)
+  #   render json: @meal, status: 200
+  # end
+  #
+  # def destroy
+  #   mealId = @meal.id
+  #   @meal.destroy
+  #   render json: {message:"Meal deleted"}
+  # end
 
 # placeholder for current meal - update with meal ID
   def current
@@ -28,9 +28,9 @@ class MealsController < ApplicationController
     render json: @meal, status: 200
   end
 
-  def show
-    render json: @meal, status: 200
-  end
+  # def show
+  #   render json: @meal, status: 200
+  # end
 
   def remove_recipe
     RecipeMeal.find_by(recipe_id: params["recipe_id"], meal_id: params["meal_id"]).destroy
@@ -43,7 +43,7 @@ class MealsController < ApplicationController
     params.permit(:id, :user_id, :name)
   end
 
-  def set_meal
-    @meal = Meal.find(params[:id])
-  end
+  # def set_meal
+  #   @meal = Meal.find(params[:id])
+  # end
 end
